32aa5e7a731955073edd3679891c49296aac1273
CuriHuS <2542656@naver.com>
2025-10-31 12:29:15 +0900

    feat(view): 당첨 번호 입력 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,12 +1,18 @@
 package lotto.view;
 
 import static lotto.view.constant.OutputMessage.REQUEST_PURCHASE_AMOUNT;
+import static lotto.view.constant.OutputMessage.REQUEST_WIN_NUMBERS;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public static String inputPurchase() {
         System.out.println(REQUEST_PURCHASE_AMOUNT);
+        return Console.readLine();
+    }
+
+    public static String inputWinNumbers() {
+        System.out.println(REQUEST_WIN_NUMBERS);
         return Console.readLine();
     }
 }
