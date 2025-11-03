27008984df5ed185fd08edf17cae0fce33f12cb0
CuriHuS <2542656@naver.com>
2025-11-03 09:52:53 +0900

    fix(view): 메시지 출력 오류 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,17 +8,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public static String inputPurchase() {
-        System.out.println(REQUEST_PURCHASE_AMOUNT);
+        System.out.println(REQUEST_PURCHASE_AMOUNT.getMessage());
         return Console.readLine();
     }
 
     public static String inputWinNumbers() {
-        System.out.println(REQUEST_WIN_NUMBERS);
+        System.out.println(REQUEST_WIN_NUMBERS.getMessage());
         return Console.readLine();
     }
 
     public static String inputBonusNumber(){
-        System.out.println(REQUEST_BONUS_NUMBER);
+        System.out.println(REQUEST_BONUS_NUMBER.getMessage());
         return Console.readLine();
     }
 }
