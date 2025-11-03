07e2dd4c1fa4df07dc8e62b09fe7ad66781f06c7
CuriHuS <2542656@naver.com>
2025-11-03 13:42:26 +0900

    refactor(constant): 로또 제한 상수 적용

--- src/main/java/lotto/model/domain/LottoFactory.java
+++ src/main/java/lotto/model/domain/LottoFactory.java
@@ -4,23 +4,20 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import lotto.model.domain.constant.LottoNumber;
 
 /**
  * 로또를 생성하는 공장 클래스입니다.
  */
 public class LottoFactory {
-    private static final int LOTTO_NUMBER_COUNT = 6;
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
-
     /**
      * 랜덤하게 로또 번호를 생성합니다.
      */
     public static Lotto createAutoLotto() {
         List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(
-                MIN_LOTTO_NUMBER,
-                MAX_LOTTO_NUMBER,
-                LOTTO_NUMBER_COUNT
+                LottoNumber.MIN_LOTTO_NUMBER,
+                LottoNumber.MAX_LOTTO_NUMBER,
+                LottoNumber.LOTTO_NUMBER_COUNT
         ));
         Collections.sort(numbers);
         return new Lotto(numbers);
